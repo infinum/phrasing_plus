c60482ce43133b3fd43b44223c2b9e31d93e04ca
Damir Svrtan <damir.svrtan@gmail.com>
2015-05-08 19:37:50 +0200

    Swap image after choosing a file and swap buttons.

--- app/assets/javascripts/phrasing_plus.js
+++ app/assets/javascripts/phrasing_plus.js
@@ -1,5 +1,7 @@
 var Phrasing = Phrasing || {};
 
+var PhrasingPlus = PhrasingPlus || {};
+
 Phrasing.isEditModeEnabled = function(){
   if($.cookie('editing_mode') === null){
     $.cookie('editing_mode', 'true');
@@ -8,12 +10,56 @@ Phrasing.isEditModeEnabled = function(){
   return $.cookie("editing_mode") === "true" ? true : false;
 };
 
+function PhrasableImageWrapper(options){
+  this.fileInput       = options.fileInput;
+  this.$fileInputLabel = options.$fileInputLabel;
+  this.$wrapper        = options.$wrapper;
+  this.$image          = options.$image;
+  this.$submitButton   = options.$submitButton;
+}
+
+PhrasableImageWrapper.prototype.handleImageUpload = function(){
+  this.rememberOriginal();
+  this.showChosenImage();
+  this.$fileInputLabel.hide();
+  this.$submitButton.show();
+};
+
+PhrasableImageWrapper.prototype.showChosenImage = function(){
+  if (this.fileInput.files && this.fileInput.files[0]) {
+    var reader = new FileReader();
+
+    var widget = this;
+
+    reader.onload = function (e) {
+      widget.$image.attr('src', e.target.result);
+    };
+
+    reader.readAsDataURL(this.fileInput.files[0]);
+  }
+};
+
+PhrasableImageWrapper.prototype.rememberOriginal = function(){
+  this.$image.data('original-src', this.$image.attr('src'));
+};
+
+
 $(document).ready(function(){
   if(Phrasing.isEditModeEnabled()){
     $('.phrasable-image-wrapper').addClass('phrasable-on');
   }
 
-  $('.phrasable-image-wrapper button').on('click', function(){
+  $('.phrasable-image-wrapper input[type=file]').change(function(){
 
+    var phrasingImageWrapper = new PhrasableImageWrapper({
+      fileInput : this,
+      $fileInputLabel : $(this).prev('label'),
+      $wrapper : $(this).closest('.phrasable-image-wrapper'),
+      $image : $(this).closest('.phrasable-image-wrapper').find('img.phrasable-image'),
+      $submitButton : $(this).closest('.phrasable-image-wrapper').find('button[type=submit]')
+    });
+
+    phrasingImageWrapper.handleImageUpload();
   });
 });
+
