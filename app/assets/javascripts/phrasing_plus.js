3dd627d411ba42b9e207c97bc107754b8fbc175a
Damir Svrtan <damir.svrtan@gmail.com>
2015-05-08 20:23:58 +0200

    Added discard change functionality

--- app/assets/javascripts/phrasing_plus.js
+++ app/assets/javascripts/phrasing_plus.js
@@ -10,22 +10,25 @@ Phrasing.isEditModeEnabled = function(){
   return $.cookie("editing_mode") === "true" ? true : false;
 };
 
-function PhrasableImageWrapper(options){
-  this.fileInput       = options.fileInput;
+function PhrasableImageWidget(options){
+  this.fileInput = options.fileInput;
   this.$fileInputLabel = options.$fileInputLabel;
-  this.$wrapper        = options.$wrapper;
-  this.$image          = options.$image;
-  this.$submitButton   = options.$submitButton;
+  this.$discardChangeLabel = options.$discardChangeLabel;
+  this.$wrapper = options.$wrapper;
+  this.$image = options.$image;
+  this.$form = options.$form;
+  this.$submitButton = options.$submitButton;
 }
 
-PhrasableImageWrapper.prototype.handleImageUpload = function(){
+PhrasableImageWidget.prototype.handleImageUpload = function(){
   this.rememberOriginal();
   this.showChosenImage();
   this.$fileInputLabel.hide();
+  this.$discardChangeLabel.show();
   this.$submitButton.show();
 };
 
-PhrasableImageWrapper.prototype.showChosenImage = function(){
+PhrasableImageWidget.prototype.showChosenImage = function(){
   if (this.fileInput.files && this.fileInput.files[0]) {
     var reader = new FileReader();
 
@@ -39,10 +42,35 @@ PhrasableImageWrapper.prototype.showChosenImage = function(){
   }
 };
 
-PhrasableImageWrapper.prototype.rememberOriginal = function(){
+PhrasableImageWidget.prototype.rememberOriginal = function(){
   this.$image.data('original-src', this.$image.attr('src'));
 };
 
+PhrasableImageWidget.prototype.discardUploadedImage = function(){
+  this.revertToOriginal();
+  this.$form[0].reset();
+  this.$fileInputLabel.show();
+  this.$discardChangeLabel.hide();
+  this.$submitButton.hide();
+};
+
+PhrasableImageWidget.prototype.revertToOriginal = function(){
+  this.$image.attr('src', this.$image.data('original-src'));
+  this.$image.removeData('original-src');
+};
+
+
+var generatePhrasableImageWidget = function($wrapper){
+  return new PhrasableImageWidget({
+    fileInput : $wrapper.find('input[type=file]')[0],
+    $fileInputLabel : $wrapper.find('label.edit-image'),
+    $discardChangeLabel : $wrapper.find('label.discard-change'),
+    $wrapper : $wrapper,
+    $image : $wrapper.find('img.phrasable-image'),
+    $form : $wrapper.find('form'),
+    $submitButton : $wrapper.find('button[type=submit]')
+  });
+};
 
 $(document).ready(function(){
   if(Phrasing.isEditModeEnabled()){
@@ -50,16 +78,13 @@ $(document).ready(function(){
   }
 
   $('.phrasable-image-wrapper input[type=file]').change(function(){
-
-    var phrasingImageWrapper = new PhrasableImageWrapper({
-      fileInput : this,
-      $fileInputLabel : $(this).prev('label'),
-      $wrapper : $(this).closest('.phrasable-image-wrapper'),
-      $image : $(this).closest('.phrasable-image-wrapper').find('img.phrasable-image'),
-      $submitButton : $(this).closest('.phrasable-image-wrapper').find('button[type=submit]')
-    });
-
+    var phrasingImageWrapper = generatePhrasableImageWidget($(this).closest('.phrasable-image-wrapper'));
     phrasingImageWrapper.handleImageUpload();
+  });
+
+  $('.phrasable-image-wrapper label.discard-change').on('click', function(){
+    var phrasingImageWrapper = generatePhrasableImageWidget($(this).closest('.phrasable-image-wrapper'));
+    phrasingImageWrapper.discardUploadedImage();
   });
 });
 
